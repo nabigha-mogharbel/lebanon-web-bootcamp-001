cfc2ac86db5d945296265f5a9c3073cbdd0a68a9
joliehobloss <jolieshobloss@gmail.com>
2020-11-18 20:27:48 +0200

    class9

---
+++ class9/madLibs/Jolie-Marwa-Abdellatif/script.js
@@ -0,0 +1,14 @@
+const el = document.getElementById("lib-button");
+let msg = "";
+
+el.addEventListener("click", function (e) {
+  let noun = document.getElementById("noun").value;
+  let adj = document.getElementById("adjective").value;
+  let person = document.getElementById("person").value;
+
+  msg = person + " really likes " + adj + " " + noun + ".";
+
+  let story = document.getElementById("story");
+  story.innerText = msg;
+
+});
